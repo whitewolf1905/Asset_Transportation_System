bef4a32814c95126c4e7bf69629383ece389ab0e
whitewolf1905 <hardikmangla195@gmail.com>
2022-11-04 18:30:00 +0530

    Api's dev complete

--- ATS/ATS_api/url.py
+++ ATS/ATS_api/url.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('show', views.show),
     path('rider_info/<int:rider_id>', views.shareRiderInfo),
     path('asset_request/<int:requester_id>', views.requesterAsset),
-    path('matched_rider/<int:requester_id>', views.matchRequests)
+    path('matched_rider/<int:requester_id>', views.matchRequests),
+    path('apply_request', views.applyRequest)
 ]
--- ATS/ATS_api/views.py
+++ ATS/ATS_api/views.py
@@ -1,15 +1,13 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
+from django.core.paginator import Paginator
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import RidersTravelInfo, RidersInfoSerializer, RequesterHistory, RequesterHistorySerializer
 
-
-@api_view(('GET',))
-def show(request):
-    return Response({"data": "Random data"})
 
 
 @api_view(['GET', 'POST'])
@@ -60,7 +58,8 @@ def requesterAsset(request, requester_id):
 
 @api_view(['GET'])
 def matchRequests(request, requester_id):
-    requester_detail = RequesterHistory.objects.filter(requester_id=requester_id)
+    requester_detail = RequesterHistory.objects.filter(requester_id=requester_id,
+                                                       transportation_status="NOT_APPLIED")
 
     if len(requester_detail):
         serialize_requester_data = RequesterHistorySerializer(requester_detail, many=True)
@@ -78,6 +77,7 @@ def matchRequests(request, requester_id):
                 if len(serialize_rider_info.data):
                     data = {
                         "Matched_Rider": serialize_rider_info.data[0]["riders_id"],
+                        "Rider_row_id": serialize_rider_info.data[0]["id"],
                         "id": each["id"],
                         "pick_from": each["pick_from"],
                         "deliver_to": each["deliver_to"],
@@ -90,7 +90,38 @@ def matchRequests(request, requester_id):
                     matched_data.append(data)
 
         if len(matched_data):
-            return Response({"data": matched_data}, status=status.HTTP_200_OK)
+
+            pagintor = Paginator(matched_data, 2)
+            try:
+                page_number = request.GET.get('page')
+                page_obj = pagintor.page(page_number)
+            except:
+                page_obj = pagintor.page(1)
+
+            data = {
+                'count': len(matched_data),
+                'previous_page': page_obj.has_previous() and page_obj.previous_page_number() or None,
+                'next_page': page_obj.has_next() and page_obj.next_page_number() or None,
+                'data': list(page_obj)
+            }
+            return Response({"data": data}, status=status.HTTP_200_OK)
 
     return Response({"message": "No data matched"}, status=status.HTTP_204_NO_CONTENT)
 
+@api_view(['POST'])
+def applyRequest(request):
+    rider_row_id = request.data["rider_row_id"]
+    requester_row_id = request.data["requester_row_id"]
+    try:
+        requester_info = RequesterHistory.objects.get(id=requester_row_id)
+        rider_info = RidersTravelInfo.objects.get(id=rider_row_id)
+        if requester_info.transportation_status == "APPLIED":
+            return Response({"message": "Request already applied"}, status=status.HTTP_200_OK)
+        rider_info.asset_quantity -= requester_info.asset
+        rider_info.save()
+        requester_info.transportation_status = "APPLIED"
+        requester_info.save()
+        return Response({"message": "Request Applied successfully"}, status=status.HTTP_200_OK)
+
+    except:
+        return Response({"message": "Couldn't apply the request"}, status=status.HTTP_400_BAD_REQUEST)
